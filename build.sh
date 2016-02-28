3c821ee8f2741fe6f12d4e3997b91b38fc8f8a25
labsin <sam.sgrs@gmail.com>
2016-02-28 14:34:45 +0100

    Add simple build scripts

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+pushd deezer
+echo "building current arch"
+go build && mv deezer x86_64-linux-gnu/deezer-scope
+echo "build current arch"
+echo "building arm"
+click chroot -a armhf -f ubuntu-sdk-15.04 -s vivid run CGO_ENABLED=1 GOARCH=arm GOARM=7 PKG_CONFIG_LIBDIR=/usr/lib/arm-linux-gnueabihf/pkgconfig:/usr/lib/pkgconfig:/usr/share/pkgconfig GOPATH=~/dev-go CC=arm-linux-gnueabihf-gcc CXX=arm-linux-gnueabihf-g++ go build -ldflags '-extld=arm-linux-gnueabihf-g++' && mv deezer arm-linux-gnueabihf/deezer-scope
+echo "build arm"
+popd
+pushd ../
+echo "building click"
+click build deezer-scope
+echo "build click"
+popd
---
+++ install.sh
@@ -0,0 +1,1 @@
+sudo click install --allow-unauthenticated --user=sam ./deezer-scope.labsin_0.1.9_multi.click
